b11221201cc3923200493b504568f5f4c84ae2f7
Fathur Rahman <arfathurrahman204@gmail.com>
2024-01-15 11:40:42 +0800

    first commit

---
+++ uganda.js
@@ -0,0 +1,11 @@
+const negara = 'uganda';
+const benua = 'afrika';
+const bahasa = "swahili";
+let populasi = 45850000;
+var kepulauan = false;
+
+console.log(`negara   :  ${negara}`);
+console.log(`benua    :  ${benua}`);
+console.log(`bahasa   :  ${bahasa}`);
+console.log(`populasi :  ${populasi}`);
+console.log(`kepulauan:  ${kepulauan}`);
