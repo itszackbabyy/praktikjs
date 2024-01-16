41fedc16ce235de35de62b6e4ce4000c6cbfadd9
Fathur Rahman <arfathurrahman204@gmail.com>
2024-01-16 15:09:02 +0800

    praktik 3

---
+++ praktik3.js
@@ -0,0 +1,6 @@
+const negara = 'uganda';
+const benua = 'afrika';
+const bahasa = "swahili";
+let populasi = 45850000;
+
+ console.log (`Negara ${negara} adalah negara yang berada di benua ${benua}, dengan populasi sebanyak ${populasi} juta jiwa, bahasa nasional ${bahasa} `);
